b1e9f60948008bfd68708746bbdc6c67a3aad70c
blackmoom2 <gideonlenge96@gmail.com>
2023-01-04 09:08:55 -0800

    0x01

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    if i % 2 == 0:
-        n = chr(i)
-    else:
-        n = chr(i-32)
-    print("{}".format(n), end="")
+    if letter % 2 != 0:
+        letter -= 32
+    print("{}".format(chr(letter)), end="")
