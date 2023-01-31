e6c08c2bd6311e5ddf567ce7e85220c7bf54e2df
blackmoom2 <gideonlenge96@gmail.com>
2023-01-31 06:12:01 -0800

    0x09

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,6 @@
+class LockedClass:
+    def __setattr__(self, attribute, value):
+        if attribute == "first_name":
+            self.__dict__[attribute] = value
+        else:
+            raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
