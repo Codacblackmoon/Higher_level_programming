3bd20fe6bd1e7b7aca7743bba2f8dca9e364a8a5
blackmoom2 <gideonlenge96@gmail.com>
2023-01-31 02:51:31 -0800

    0x08

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -4,6 +4,11 @@
 creating an emtye rectangle class
 and diffining it
 '''
+
 class Rectangle:
+     """Class of a Rectangle"""
+
     def __init__(self):
+        """Initialize class"""
+
         pass
