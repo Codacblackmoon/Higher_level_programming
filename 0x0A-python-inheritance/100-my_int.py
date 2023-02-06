1090831ccab714fb245e757661165e1585bcb223
blackmoom2 <gideonlenge96@gmail.com>
2023-02-06 01:19:03 -0800

    0x0A

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,9 @@
+class MyInt(int):
+     def __init__(self, num):
+         self.num = num
+
+    def __eq__(self, other):
+        return self.num != other
+
+    def __ne__(self, other):
+        return self.num == other
