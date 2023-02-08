fe81d0c5b253ee6eae4c182f528bc84e4b7eaad0
blackmoom2 <gideonlenge96@gmail.com>
2023-02-08 12:29:50 -0800

    0x0b

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, mode="r+", encoding="utf-8") as f:
+        new_text = ""
+        for line in f:
+            new_text += line
+            if search_string in line:
+                new_text += new_string
+        f.seek(0)
+        f.write(new_text)
